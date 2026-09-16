338b0c483f37eb09056018c1c2e133f6aa94f0cc
Akhil Anand <ad.elos.corp@gmail.com>
2026-09-16 07:23:54 +0530

    Rollback Holistic shutdown hook

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -163,7 +163,6 @@ class MainWindow(QMainWindow):
     def closeEvent(self, event):
         self.timer.stop()
         self.gesture.close()
-        self.invisibility.close()
         self.camera.stop()
         event.accept()
 
